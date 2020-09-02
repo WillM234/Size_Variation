ece7ad41a9e1ae523eff6d39f9a03444a07c4b92
Will M <willmcb234@gmail.com>
2020-09-01 20:14:32 -0400

    Unity Project Upload and Packages

---
+++ SizeVariation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private float thrust = 100f;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))//move right
+            {
+            rb.AddForce(transform.right * speed);
+        }
+        if(Input.GetKey(KeyCode.A))// move left
+            {
+            rb.AddForce(-transform.right * speed);
+            }
+        if(Input.GetKeyDown(KeyCode.Space))//jump control
+        {
+            rb.AddForce(transform.up * thrust);
+        }
+    }
+}
---
+++ SizeVariation/Assets/Scripts/SizeVariation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeVariation : MonoBehaviour
+{
+    private Vector3 ScaleChangePositive, ScaleChangeNegative;
+    public GameObject Player;
+    void Awake()
+    {
+        ScaleChangePositive = new Vector3(0.1f, 0.1f, 0.1f);//sets value to change of scale increase
+        ScaleChangeNegative = new Vector3(-0.1f, -0.1f, -0.1f);// sets value change of scale decrease
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "SizeIncreaser")// increases local scale
+        {
+            Player.transform.localScale += ScaleChangePositive;
+        }
+        else if(other.gameObject.tag == "SizeDecreaser")// decreases loacl scale
+        {
+            Player.transform.localScale += ScaleChangeNegative;
+        }
+    }
+}
